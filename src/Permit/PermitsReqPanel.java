9b188cb597bf13a55f3c2b1cf23883735658e299
irbyn <31327527+irbyn@users.noreply.github.com>
2017-09-20 18:11:35 +1200

    Signed-off-by: irbyn <31327527+irbyn@users.noreply.github.com>

--- src/Permit/PermitsReqPanel.java
+++ src/Permit/PermitsReqPanel.java
@@ -5,19 +5,44 @@ import java.awt.EventQueue;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 class PermitsReqPanel extends JPanel {
+	
+	private JTable permitsTbl;
 
 	  public PermitsReqPanel() {
+		  	setLayout(null);	  
+		  
+	        DefaultTableModel model = new DefaultTableModel();
+	        model.addColumn("Code");
+	        model.addColumn("Name");
+	        model.addColumn("Quantity");
+	        model.addColumn("Unit Price");
+	        model.addColumn("Price");
+	        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3","Column 4","Column 5"});
+	        model.addRow(new Object[]{"Column 1", "Column 2", "Column 2","Column 4","Column 5"});
+	        model.addRow(new Object[]{"Column 1", "Column 2", "Column 4","Column 4","Column 5"});
+	        model.addRow(new Object[]{"Column 1", "Column 2", "Column 1","Column 4","Column 5"});
+	        model.addRow(new Object[]{"Column 1", "Column 2", "Column 99","Column 4","Column 5"});
+	        
+	        JTable table = new JTable(model);
+		  	
 
-	    JButton b1 = new JButton("New York");
-	    add(b1);
-	    JButton b2 = new JButton("London");
-	    add(b2);
-	    JButton b3 = new JButton("Hong Kong");
-	    add(b3);
-	    JButton b4 = new JButton("Tokyo");
-		    add(b4);
+	//	  permitsTbl.setColumns(10);
+	        table.setBounds(20, 20, 800, 200);
+		  	add(table);
+	    
+
+		 // Create a couple of columns 
+		//  	model.addColumn("Col1"); 
+		//  	model.addColumn("Col2"); 
+
+		  	// Append a row 
+		//  	model.addRow(new Object[]{"v1", "v2"});
+	    
 		}
 	
 }
